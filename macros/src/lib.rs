73e6934aae01a67699de003f1384241401a1b893
Bruce <BruceChar@163.com>
2023-10-16 15:12:07 +0800

    done macros

--- macros/src/lib.rs
+++ macros/src/lib.rs
@@ -9,3 +9,13 @@ macro_rules! hashmap {
         }
     }
 }
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test_invalid() {
+        let _hm1: std::collections::HashMap<_, _> = hashmap!('a' => 1, 'b' => 2);
+        let _hm2: std::collections::HashMap<_, _> = hashmap!('a' => 1,'b'=>2);
+        print!("-----{:?}\n{:?}", _hm1, _hm2);
+    }
+}
