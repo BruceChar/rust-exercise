a31136685d68474c8fd6a4106a499d1de97179f6
Bruce <BruceChar@163.com>
2023-10-19 00:20:15 +0800

    forth todo define

--- forth/src/lib.rs
+++ forth/src/lib.rs
@@ -3,13 +3,14 @@ use std::collections::HashMap;
 use once_cell::sync::Lazy;
 
 pub type Value = i32;
-pub type Result = std::result::Result<(), Error>;
+pub type Result<T> = std::result::Result<T, Error>;
+type UnitResult = Result<()>;
 
-type Fop = fn(&mut Forth) -> Result;
+type Fop = fn(&mut Forth) -> UnitResult;
 
 const LOOKUP: [&str; 8] = ["+", "-", "*", "/", "dup", "swap", "over", "drop"];
 static OPMAP: Lazy<HashMap<&str, Fop>> = Lazy::new(|| {
-    let mut map: HashMap<&str, fn(&mut Forth) -> Result> = HashMap::new();
+    let mut map: HashMap<&str, Fop> = HashMap::with_capacity(LOOKUP.len());
     LOOKUP.iter().for_each(|&op| {
         let call = match op {
             "+" => Forth::add,
@@ -30,7 +31,23 @@ static OPMAP: Lazy<HashMap<&str, Fop>> = Lazy::new(|| {
 #[derive(Debug, Clone)]
 enum Op {
     Call(Fop),
-    Num(i32)
+    Num(i32),
+    Ops(Vec<Op>),
+}
+
+impl Op {
+    fn call(&mut self, forth: &mut Forth) -> UnitResult {
+        match self {
+            Op::Call(call) => call(forth),
+            Op::Num(num) => forth.push(*num),
+            Op::Ops(ops) => {
+                for op in ops {
+                    op.call(forth)?;
+                }
+                Ok(())
+            }
+        }
+    }
 }
 
 pub struct Forth {
@@ -58,11 +75,11 @@ impl Forth {
         &self.stack
     }
 
-    fn pop(&mut self) -> std::result::Result<Value, Error> {
+    fn pop(&mut self) -> Result<Value> {
         self.stack.pop().ok_or(Error::StackUnderflow)
     }
 
-    fn push(&mut self, val: Value) -> Result {
+    fn push(&mut self, val: Value) -> UnitResult {
         self.stack.push(val);
         Ok(())
     }
@@ -71,30 +88,30 @@ impl Forth {
         self.stack.len()
     }
 
-    fn add(&mut self) -> Result {
+    fn add(&mut self) -> UnitResult {
         self.arth(|a, b| a + b)
     }
-    fn sub(&mut self) -> Result {
+    fn sub(&mut self) -> UnitResult {
         self.arth(|a, b| a - b)
     }
-    fn mul(&mut self) -> Result {
+    fn mul(&mut self) -> UnitResult {
         self.arth(|a, b| a * b)
     }
-    fn div(&mut self) -> Result {
+    fn div(&mut self) -> UnitResult {
         self.arth(|a, b| a / b)
     }
 
-    fn drop(&mut self) -> Result {
+    fn drop(&mut self) -> UnitResult {
         self.stack.pop().ok_or(Error::StackUnderflow)?;
         Ok(())
     }
 
-    fn dup(&mut self) -> Result {
+    fn dup(&mut self) -> UnitResult {
         let v = self.stack.last().ok_or(Error::StackUnderflow)?;
         self.push(*v)
     }
 
-    fn over(&mut self) -> Result {
+    fn over(&mut self) -> UnitResult {
         if self.len() < 2 {
             return Err(Error::StackUnderflow);
         }
@@ -105,7 +122,7 @@ impl Forth {
         self.push(*v)
     }
 
-    fn swap(&mut self) -> Result {
+    fn swap(&mut self) -> UnitResult {
         let len = self.stack.len();
         if len < 2 {
             return Err(Error::StackUnderflow);
@@ -114,7 +131,7 @@ impl Forth {
         Ok(())
     }
 
-    fn arth(&mut self, f: fn(i32, i32) -> i32) -> Result {
+    fn arth(&mut self, f: fn(i32, i32) -> i32) -> UnitResult {
         let (v1, v2) = (self.pop()?, self.pop()?);
         if v1 == 0 && f(2, 2) == 1 {
             return Err(Error::DivisionByZero);
@@ -122,19 +139,24 @@ impl Forth {
         self.push(f(v2, v1))
     }
 
-    pub fn eval(&mut self, input: &str) -> Result {
+    pub fn eval(&mut self, input: &str) -> UnitResult {
         let mut iter = input.split_whitespace();
         while let Some(s) = iter.next() {
             let s = s.to_ascii_lowercase();
             let s = s.as_str();
             if let Some(f) = self.ops.get(s) {
                 match f {
-                    Op::Call(f) =>  f(self)?,
+                    Op::Call(f) => f(self)?,
                     Op::Num(i) => self.push(*i)?,
+                    Op::Ops(ops) => {
+                        for op in ops {
+                            f.call(*self)?;
+                        }
+                    },
                 }
                 continue;
             }
-            // ops can be rewrite
+            // ops can be rewrite, must be after self.ops
             if let Some(f) = OPMAP.get(s) {
                 f(self)?;
                 continue;
@@ -144,38 +166,46 @@ impl Forth {
                 continue;
             }
             if s == ":" {
-                println!("{:?}", iter);
-                let w = iter.next().ok_or(Error::StackUnderflow)?;
-                if str::parse::<i32>(w).is_ok() {
-                    return Err(Error::InvalidWord);
-                }
-                let op = iter.next().ok_or(Error::StackUnderflow)?;
-                let c = iter.next().ok_or(Error::StackUnderflow)?;
-                if c != ";" {
-                    return Err(Error::InvalidWord);
-                }
-                
-                let f: Op = match op {
-                    "dup" => Op::Call(Self::dup),
-                    "swap" => Op::Call(Self::swap),
-                    "drop" => Op::Call(Self::drop),
-                    "over" => Op::Call(Self::over),
-                    _ => {
-                        if let Ok(i) = str::parse::<i32>(op) {
-                            Op::Num(i)
-                        } else if let Some(f) = self.ops.get(op) {
-                            f.clone()
-                        } else {
-                            return Err(Error::UnknownWord);
-                        }
-                    },
-                };
-                self.ops.insert(w.to_owned(), f);
+                let (w, op) = self.define(&mut iter, &self.ops)?;
+                self.ops.insert(w, op);
             } else {
                 return Err(Error::UnknownWord);
             }
         }
         Ok(())
+    }
+
+    fn define<'a>(&self,
+        iter: &mut impl Iterator<Item = &'a str>,
+        ops: &HashMap<String, Op>,
+    ) -> Result<(String, Op)> {
+        let w = iter.next().ok_or(Error::StackUnderflow)?;
+        if str::parse::<i32>(w).is_ok() {
+            return Err(Error::InvalidWord);
+        }
+        let mut ops = vec![];
+        while let Some(s) = iter.next() {
+            match s {
+                // Assume the ";" is the last
+                ";" => return Ok((w.to_owned(), Op::Ops(ops))),
+                _ => {
+                    if let Some(f) = OPMAP.get(s) {
+                        ops.push(Op::Call(*f));
+                        continue;
+                    }
+                    if let Ok(num) = str::parse::<i32>(s) {
+                        ops.push(Op::Num(num));
+                        continue;
+                    } 
+                    if let Some(f) = self.ops.get(s) {
+                        ops.push(f.clone());
+                    } else {
+                        return Err(Error::UnknownWord);
+                    }
+                }
+            }
+        }
+        return Err(Error::StackUnderflow);
     }
 }
 
