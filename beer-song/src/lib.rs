6f98cbae6fdf9e3e54925bd719343a0e518d87d3
Bruce <BruceChar@163.com>
2023-10-21 01:08:52 +0800

    beer-song

---
+++ beer-song/src/lib.rs
@@ -0,0 +1,18 @@
+pub fn verse(n: u32) -> String {
+    match n {
+        0 => format!("No more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.\n"),
+        1 => format!("1 bottle of beer on the wall, 1 bottle of beer.\nTake it down and pass it around, no more bottles of beer on the wall.\n"),
+        _ => {
+            let bottle = if n == 2 { "bottle" } else { "bottles" };
+            format!("{n} bottles of beer on the wall, {n} bottles of beer.\nTake one down and pass it around, {} {bottle} of beer on the wall.\n", n - 1)
+        }
+    }
+}
+
+pub fn sing(start: u32, end: u32) -> String {
+    let mut s = verse(start);
+    for n in (end..start).rev() {
+        s += format!("\n{}", verse(n)).as_str();
+    }
+    s
+}
