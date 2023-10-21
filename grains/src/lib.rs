edae76c31111889446ac081834075d669d1d4f91
Bruce <BruceChar@163.com>
2023-10-21 23:08:12 +0800

    grains

---
+++ grains/src/lib.rs
@@ -0,0 +1,11 @@
+pub fn square(s: u32) -> u64 {
+    if s > 64 || s == 0 {
+        panic!("Square must be between 1 and 64")
+    }
+    2u64.pow(s - 1)
+}
+
+pub fn total() -> u64 {
+    // 18_446_744_073_709_551_615
+    (1..=64).fold(0, |acc, s| acc + square(s))
+}
