940b36c8fbb0c73e2b7b05d4581fcde000468ac7
Bruce <BruceChar@163.com>
2023-10-23 14:10:05 +0800

    optimize proverb

--- proverb/src/lib.rs
+++ proverb/src/lib.rs
@@ -1,12 +1,12 @@
 pub fn build_proverb(list: &[&str]) -> String {
-    if list.is_empty() {
-        return "".to_string();
+    match list.first() {
+        None => "".to_string(),
+        Some(word) => list
+            .windows(2)
+            .map(|s| format!("For want of a {} the {} was lost.\n", s[0], s[1]))
+            .chain(std::iter::once(format!(
+                "And all for the want of a {word}."
+            )))
+            .collect(),
     }
-    let last = format!("And all for the want of a {}.", list[0]);
-    let mut v = vec![];
-    for s in list.windows(2) {
-        v.push(format!("For want of a {} the {} was lost.", s[0], s[1]));
-    }
-    v.push(last);
-    v.join("\n")
 }
