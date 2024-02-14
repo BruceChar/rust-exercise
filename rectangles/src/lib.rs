79758af063f3ffc5b66a41f3bb834bbe2f7e0868
Bruce <BruceChar@163.com>
2024-02-15 00:58:44 +0800

    fix rectangle count

--- rectangles/src/lib.rs
+++ rectangles/src/lib.rs
@@ -1,34 +1,29 @@
-use std::cmp::Ordering;
-
 pub fn count(lines: &[&str]) -> u32 {
     if lines.len() < 2 {
         return 0;
     }
-    let mut map: Vec<Vec<usize>> = vec![];
-    for l in lines.iter() {
-        let mut t = vec![];
-        l.chars().enumerate().for_each(|(j, c)| {
-            if c == '+' {
-                t.push(j)
-            }
-        });
-        map.push(t);
-    }
 
     let mut sum = 0;
-    for i in 0..map.len() - 1 {
-        for j in i + 1..map.len() {
+    for i in 0..lines.len() - 1 {
+        for j in i + 1..lines.len() {
             let mut dot = 0;
-            let mut p = 0;
-            let mut q = 0;
-            while p < map[i].len() && q < map[j].len() {
-                match map[i][p].cmp(&map[j][q]) {
-                    Ordering::Less => p += 1,
-                    Ordering::Greater => q += 1,
-                    Ordering::Equal => {
-                        dot += 1;
-                        p += 1;
-                        q += 1;
+            for (k, (a, b)) in lines[i].chars().zip(lines[j].chars()).enumerate() {
+                match (a, b) {
+                    ('+', '+') => {
+                        let h = (i + 1..j)
+                            .filter(|ind| {
+                                let c = lines[*ind].chars().nth(k);
+                                c == Some('|') || c == Some('+')
+                            })
+                            .count();
+                        if h == j - i - 1 {
+                            dot += 1;
+                        }
+                    }
+                    ('-' | '+', '-' | '+') => (),
+                    _ => {
+                        sum += if dot > 0 { (dot - 1) * (dot) / 2 } else { 0 };
+                        dot = 0; // reset after count
                     }
                 }
             }
